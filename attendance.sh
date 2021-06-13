96d93d02ccd6e5c14ae577857267bb8ee084acaa
CraftyChimera <76778532+CraftyChimera@users.noreply.github.com>
2021-06-13 23:11:54 +0530

    Update attendance.sh

--- attendance.sh
+++ attendance.sh
@@ -1,13 +1,13 @@
 d=${1:-2019-09-27}
 a=${2:-2021-10-27}
 y=$(date --date="$a+1 day" +%Y-%m-%d)
-while [ "$d" != "$y" ]; do 
- if [ $(grep $d ./attendance.txt | wc -l) -gt 0 ]
+while [ "$d" != "$y" ]; do  #loop for iterating through dates
+ if [ $(grep $d ./attendance.txt | wc -l) -gt 0 ] #check if there is a meeting on a day. If so,count the number of people who don't appear
  then
  
   for i in {1..9}
    do
-   if [ $(grep $d ./attendance.txt | grep webDev_0$i|wc -l) == 0 ]
+   if [ $(grep $d ./attendance.txt | grep webDev_0$i|wc -l) == 0 ] #take note of people who don't appear in attendance. repeat throughout program
    then
    echo -e "webDev_0$i \t $d"
    fi
