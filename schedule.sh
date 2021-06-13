ae37c7391a8306c1f6d27080080101ef46998941
CraftyChimera <76778532+CraftyChimera@users.noreply.github.com>
2021-06-13 15:39:00 +0530

    Add files via upload

---
+++ schedule.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+grep $(date +%F) /home/drake/Desktop/future.txt >> /home/Jay_Jay/schedule.txt
+for i in {1..30}
+do
+if [ $i -lt 10 ]
+then
+ln -v /home/Jay_Jay/schedule.txt /home/sysAd_0$i/
+ln -v /home/Jay_Jay/schedule.txt /home/webDev_0$i/
+ln -v /home/Jay_Jay/schedule.txt /home/appDev_0$i/
+else
+ln -v /home/Jay_Jay/schedule.txt /home/sysAd_$i/
+ln -v /home/Jay_Jay/schedule.txt /home/webDev_$i/
+ln -v /home/Jay_Jay/schedule.txt /home/appDev_$i/
+fi
+done
